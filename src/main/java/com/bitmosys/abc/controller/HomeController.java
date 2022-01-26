e99a7f9bc5fa90e77d8ed86c3ceaec06b4b47277
codeChef8888 <eryashre7777@gmail.com>
2022-01-26 18:34:52 +0545

    Intial Commit

---
+++ src/main/java/com/bitmosys/abc/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.bitmosys.abc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
